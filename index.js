924aa5481df79480644303243bee5d25646eae49
Sarath Kumar Dunga <sarathkumardunga09@gmail.com>
2024-05-25 12:43:32 -0700

    Added userSchema, created api's

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const cors = require('cors');
 const PORT = 8000;
 require('dotenv').config();
 
+const app = express();
 //Create an instance of a database
 require('./db');
-const app = express();
+
+const User = require('./MODELS/UserSchema');
 
 app.get('/', (req, res) => {
     res.send('The API is working fine.');
 });
 
+//Create register API
+app.post('/register', (req, res) => {
+    try{
+        const {name, password, email, age, gender} = req.body;
+    }
+    catch(err){
+        res.status(500).json({message: err.message})
+    }
+});
+
 app.listen(PORT, (req, res) => {
     console.log(`Server is running on PORT ${PORT}`);
-})
+});
